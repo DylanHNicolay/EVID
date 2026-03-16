9489fb0d3219afaa120cab535c18bc45ffe7803c
ashley <ashleychan1353@gmail.com>
2026-03-16 15:33:11 -0400

    pages in profile

--- divecloud-react/src/App.tsx
+++ divecloud-react/src/App.tsx
@@ -3,22 +3,13 @@ import ProfileCard from './components/profile/ProfileCard';
 import TeamCard from './components/profile/TeamCard';
 import Header from './components/header/Header';
 import LatestResults from './components/profile/LatestResults';
+import ProfilePage from './pages/profilePage';
 
 function App(): React.ReactElement {
   return (
     <div className="App">
       <Header />
-      <ProfileCard
-        firstName="Chris"
-        lastName="Guiliano"
-        location="Douglassville, PA"
-        team="Longhorn Aquatics"
-        avatarUrl="https://i.pravatar.cc/150"
-      />
-      <TeamCard />
-      <div style={{ maxWidth: 900, margin: '0 auto', padding: '0 20px' }}>
-        <LatestResults />
-      </div>
+      <ProfilePage/>
     </div>
   );
 }
---
+++ divecloud-react/src/pages/profilePage.tsx
@@ -0,0 +1,27 @@
+import ProfileCard from "../components/profile/ProfileCard";
+import React, { useState } from 'react';
+import ProfileHomeTab from "../tabs/profile-home-tab";
+import ProfileScoresTab from "../tabs/profile-scores-tab";
+import ProfileMeetsTab from "../tabs/profile-meets-tab";
+
+export default function ProfilePage(){
+    const [activeTab, setActiveTab] = useState('Home');
+    return (
+        <div>
+            <ProfileCard
+                firstName="Chris"
+                lastName="Guiliano"
+                location="Douglassville, PA"
+                team="Longhorn Aquatics"
+                avatarUrl="https://i.pravatar.cc/150"
+                activeTab={activeTab}
+                onTabChange={setActiveTab}
+            />
+
+            {activeTab === 'Home' && <ProfileHomeTab />}
+            {activeTab === 'Meets' && <ProfileMeetsTab />}
+            {activeTab === 'Scores' && <ProfileScoresTab />}
+        </div>
+        
+    )
+}
